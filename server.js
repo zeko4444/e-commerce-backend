fd6aaaa4aa0d34aa7d67ff5cc4fc547af0024625
ahmed elzeny <elzeny08@gmail.com>
2025-04-27 23:05:58 +0300

    deploy app

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const cors = require("cors");
+const compression = require("compression");
 dotenv.config({ path: "config.env" });
 const ApiError = require("./utils/apiError");
 const globalError = require("./middlewares/errorMiddleware");
@@ -11,6 +13,13 @@ database();
 
 // express app
 const app = express();
+
+// Enable other domains to access your application
+app.use(cors());
+app.options("*", cors());
+
+// compress all responses
+app.use(compression());
 
 //Middlewares
 app.use(express.json());
